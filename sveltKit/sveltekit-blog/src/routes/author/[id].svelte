39405a2c56c22707b04eef2977209d32ad730d4d
Aziza Alhouni <azizaalhouni@Azizas-MacBook-Air.local>
2022-01-31 12:30:08 -0600

    svelte

---
+++ sveltKit/sveltekit-blog/src/routes/author/[id].svelte
@@ -0,0 +1,35 @@
+<script context="module">
+  export const load = async ({ params, fetch }) => {
+    console.log(params);
+    const id = params.id;
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+    const author = await res.json();
+
+    return {
+      props: {
+        author,
+      },
+    };
+  };
+</script>
+
+<script>
+  export let author;
+</script>
+
+<h1>Author Info</h1>
+
+<h1>{author.name}</h1>
+
+<p>Website: {author.website}</p>
+<p>Company Catch phrase: {author.company.catchPhrase}</p>
+
+<style>
+  h1 {
+    margin-bottom: 30px;
+  }
+  p {
+    margin-left: 80px;
+  }
+</style>
---
+++ sveltKit/sveltekit-blog/src/routes/watch.svelte
@@ -0,0 +1,1 @@
+<h1>Apple Watch</h1>
